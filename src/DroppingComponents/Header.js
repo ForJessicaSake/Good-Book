5f76bc1aa52f78a64a27f8261e7540b9ad5d839d
Jessica Joseph <jessicajoseph2019@yahoo.com>
2022-11-12 06:01:05 +0100

    feat:completed the queries for header and author

--- src/DroppingComponents/Header.js
+++ src/DroppingComponents/Header.js
@@ -5,9 +5,9 @@ import {GrPrevious} from 'react-icons/gr'
 class Header extends React.Component {
   render() {
     return (
-      <section className="lg:ml-10 ml-4 pt-8 lg:flex lg:justify-between grid grid-cols-1 ">
-        <figcaption className='flex flex-col justify-center'>
-          <h1 className="text-4xl font-extrabold lg:w-44 w-96 tracking-wide mb-8">
+      <section className="lg:ml-10 ml-2 pt-8 lg:flex lg:justify-between grid grid-cols-1 ">
+        <figcaption className='flex flex-col justify-center ml-2'>
+          <h1 className="lg:text-4xl text-4xl font-extrabold lg:w-44 w-96 tracking-wide mb-8">
             Discover the world with new Book
           </h1>
           <p className="w-56 text-sm text-red">
@@ -21,12 +21,12 @@ class Header extends React.Component {
         <p className=' mt-36 w-20 ml-48 text-red text-sm hidden lg:block'>1008 books in library</p>
 
         <figure className="py-6 w-96 bg-yellow lg:w-6/12 flex justify-center cursor-pointer lg:mt-0 ">
-            <div className='flex justify-center items-center shadow-xl bg-white h-20 w-20 relative top-52 lg:right-16 right-6'>
+            <div className='flex justify-center items-center shadow-xl bg-white h-20 w-20 relative top-52 lg:right-16 right-6 rounded-sm'>
             <GrPrevious className='text-3xl'/>
             </div>
           <figure>
-            <div className="bg-red w-24 h-24 lg:h-36 lg:w-36 rounded-full mt-72 lg:mr-16 relative top-20"></div>
-            <h1 className='lg:text-4xl text-3xl font-extrabold w-20 relative lg:left-20 left-10'>Best Sellers</h1>
+            <div className="bg-red w-24 h-24 lg:h-36 lg:w-36 rounded-full mt-72 lg:mr-16 relative top-20 right-10"></div>
+            <h1 className='lg:text-4xl text-3xl font-extrabold w-20 relative lg:left-20'>Best Sellers</h1>
           </figure>
           <figure>
             <img
